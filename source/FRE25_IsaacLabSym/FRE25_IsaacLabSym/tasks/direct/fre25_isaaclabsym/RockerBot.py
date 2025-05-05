c13bb86a87745213076429f15b45c7561623c3f6
PaoloGinefra <ginefra.paolo@gmail.com>
2025-05-05 17:52:56 +0200

    feat : super basic version

---
+++ source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/RockerBot.py
@@ -0,0 +1,77 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the leatherback robot."""
+
+import os
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets import ArticulationCfg
+
+# Get absolute path to workspace root
+WORKSPACE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+# USD path with proper resolution for cross-platform compatibility
+USD_PATH = os.path.join(WORKSPACE_ROOT, "Collected_RockerBot", "RockerBot.usd")
+
+wheelsJoints = [
+    "front_left_wheel_joint",
+    "front_right_wheel_joint",
+    "rear_left_wheel_joint",
+    "rear_right_wheel_joint"
+]
+
+steeringJoints = [
+    "front_left_steer_joint",
+    "front_right_steer_joint",
+    "rear_left_steer_joint",
+    "rear_right_steer_joint"
+]
+
+
+ROCKERBOT_CFG: ArticulationCfg = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=USD_PATH,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            rigid_body_enabled=True,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=100.0,
+            enable_gyroscopic_forces=True,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(2.0, 2.0, 0.05),
+        joint_pos={
+            jointName: 0.0 for jointName in steeringJoints
+        },
+        joint_vel={
+            jointName: 0.0 for jointName in wheelsJoints
+        },
+    ),
+    actuators={
+        "wheels": ImplicitActuatorCfg(
+            joint_names_expr=[".*wheel_joint"],
+            effort_limit=40000.0,
+            velocity_limit=500.0,
+            stiffness=0.0,
+            damping=100000.0,
+        ),
+        "steering": ImplicitActuatorCfg(
+            joint_names_expr=[".*steer_joint"],
+            effort_limit=40000.0,
+            velocity_limit=100.0,
+            stiffness=10000.0,
+            damping=10.0,
+        ),
+    },
+)
--- source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/fre25_isaaclabsym_env_cfg.py
+++ source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/fre25_isaaclabsym_env_cfg.py
@@ -10,6 +10,7 @@ from isaaclab.envs import DirectRLEnvCfg
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sim import SimulationCfg
 from isaaclab.utils import configclass
+from .RockerBot import ROCKERBOT_CFG, wheelsJoints, steeringJoints
 
 
 @configclass
@@ -18,25 +19,27 @@ class Fre25IsaaclabsymEnvCfg(DirectRLEnvCfg):
     decimation = 2
     episode_length_s = 5.0
     # - spaces definition
-    action_space = 1
-    observation_space = 4
+    action_space = 8
+    observation_space = 8
     state_space = 0
 
     # simulation
     sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
 
     # robot(s)
-    robot_cfg: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+    robot_cfg: ArticulationCfg = ROCKERBOT_CFG.replace(prim_path="/World/envs/env_.*/Robot")
 
     # scene
-    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=16, env_spacing=10.0, replicate_physics=True)
 
     # custom parameters/scales
     # - controllable joint
     cart_dof_name = "slider_to_cart"
     pole_dof_name = "cart_to_pole"
+    wheels_dofs_names = wheelsJoints
+    steering_dofs_names = steeringJoints
     # - action scale
-    action_scale = 100.0  # [N]
+    action_scale = 100000.0  # [N]
     # - reward scales
     rew_scale_alive = 1.0
     rew_scale_terminated = -2.0
