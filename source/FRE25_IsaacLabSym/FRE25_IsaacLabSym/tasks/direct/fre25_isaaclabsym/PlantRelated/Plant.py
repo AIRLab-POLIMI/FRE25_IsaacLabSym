7d13f02d6223b0f3ce8a74c6c016158d5a226048
PaoloGinefra <ginefra.paolo@gmail.com>
2025-05-12 18:13:08 +0200

    feat: basic random plants

--- source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/PlantRelated/Plant.py
+++ source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/PlantRelated/Plant.py
@@ -5,6 +5,6 @@ import isaaclab.sim as sim_utils
 WORKSPACE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 # USD path with proper resolution for cross-platform compatibility
-USD_PATH = os.path.join(WORKSPACE_ROOT, "Assets", "Plant.usd")
+USD_PATH = os.path.join(WORKSPACE_ROOT, "..", "Assets", "Plant.usd")
 
-PLANT_CFG = sim_utils.UsdFileCfg(usd_path=USD_PATH, scale=(0.03, 0.03, 0.03))
+PLANT_CFG = sim_utils.UsdFileCfg(usd_path=USD_PATH, scale=(0.03, 0.03, 0.03), rigid_props=sim_utils.RigidBodyPropertiesCfg(kinematic_enabled=True))
