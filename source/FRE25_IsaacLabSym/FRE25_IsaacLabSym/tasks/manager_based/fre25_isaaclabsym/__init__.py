b0ca87c1cf122e8019d5bac399dba672601342c0
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-07-03 16:26:53 +0200

    Initial commit

---
+++ source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/manager_based/fre25_isaaclabsym/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Fre25-Isaaclabsym-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.fre25_isaaclabsym_env_cfg:Fre25IsaaclabsymEnvCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
