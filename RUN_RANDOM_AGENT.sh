1666faffbefd311a732d5c995dba3a7fad1e1b31
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-09-14 21:25:08 +0200

    feat: add headless mode

--- RUN_RANDOM_AGENT.sh
+++ RUN_RANDOM_AGENT.sh
@@ -1,1 +1,29 @@
-python scripts/random_agent.py --task=Fre25-Isaaclabsym-Direct-v0 --num_envs 2
+HEADLESS = false
+# get options from command line
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --headless)
+      HEADLESS=true
+      shift
+      ;;
+    -h|--help)
+      echo "Usage: $0 [--headless]"
+      exit 0
+      ;;
+    --) # end of options
+      echo sus
+      shift
+      break
+      ;;
+    *)
+      break
+  esac
+done
+
+if [ "$HEADLESS" = true ]; then
+  echo "Running in headless mode"
+  python scripts/random_agent.py --task=Fre25-Isaaclabsym-Direct-v0 --num_envs 128 --headless
+else
+  echo "Running in GUI mode"
+  python scripts/random_agent.py --task=Fre25-Isaaclabsym-Direct-v0 --num_envs 2
+fi
