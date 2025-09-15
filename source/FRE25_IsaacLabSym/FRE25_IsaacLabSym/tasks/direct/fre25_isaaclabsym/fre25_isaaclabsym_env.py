e24310fe10c7246ecd7daa3400a46e68cdfc7969
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-09-15 17:06:55 +0200

    param: decrease padding to 0.5 from 0.7

--- source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/fre25_isaaclabsym_env.py
+++ source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/fre25_isaaclabsym_env.py
@@ -122,7 +122,7 @@ class Fre25IsaaclabsymEnv(DirectRLEnv):
                 self.paths.pathLength,
                 1.5 * self.paths.pathsSpacing * (self.commandBuffer.maxRows),
             ),
-            endOfRowPadding=0.7,
+            endOfRowPadding=0.5,
             waypointsPerRow=6,
         )
         self.waypoints.initializeWaypoints()
