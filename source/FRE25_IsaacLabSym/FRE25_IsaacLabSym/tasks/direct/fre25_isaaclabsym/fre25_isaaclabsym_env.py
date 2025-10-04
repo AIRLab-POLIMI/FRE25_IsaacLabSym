e5e76829e9c52c219949f549f529cee19177fe74
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-10-04 16:52:54 +0200

    param: increase end of row padding to 0.3 from 0.2

--- source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/fre25_isaaclabsym_env.py
+++ source/FRE25_IsaacLabSym/FRE25_IsaacLabSym/tasks/direct/fre25_isaaclabsym/fre25_isaaclabsym_env.py
@@ -147,7 +147,7 @@ class Fre25IsaaclabsymEnv(DirectRLEnv):
                 self.paths.pathLength,
                 1.5 * self.paths.pathsSpacing * (self.commandBuffer.maxRows),
             ),
-            endOfRowPadding=0.2,
+            endOfRowPadding=0.3,
             waypointsPerRow=10,
         )
         self.waypoints.initializeWaypoints()
